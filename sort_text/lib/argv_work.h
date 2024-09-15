d412610aef97b2731603babd83633b47882d99ed
71frukt <71frukt@mail.ru>
2024-09-15 13:52:11 +0300

    GetPath -> prog opens folder whith TXTs

--- sort_text/lib/argv_work.h
+++ sort_text/lib/argv_work.h
@@ -7,6 +7,11 @@
 
 #define GET_PATH(PATH, NAME) PATH NAME
 
+const int MAX_PATH_LEN = 100;
+const int MAX_FOLDNAME_LEN = 20;
+
+const char FOLD_NAME [MAX_FOLDNAME_LEN] = "texts/";
+
 enum OpenFilesRes
 {
     OPEN_OK,
@@ -15,5 +20,6 @@ enum OpenFilesRes
 };
 
 OpenFilesRes OpenGettedFiles(const int argc, const char *argv[], FILE **file_from, FILE **file_to);
+void GetPath(char *res_path, const char *folder, const char *name, size_t max_foldname_len, size_t max_path_name);
 
 #endif
--- sort_text/src/argv_work.cpp
+++ sort_text/src/argv_work.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 #include "argv_work.h"
 
@@ -10,8 +11,16 @@ OpenFilesRes OpenGettedFiles(const int argc, const char *argv[], FILE **file_fro
     assert(file_to);
     assert(file_from != file_to);
 
-    *file_from = fopen(argv[1], "r");   
-    *file_to = fopen(argv[2], "w+");    
+    char path_from[MAX_PATH_LEN] = {};
+    char path_to[MAX_PATH_LEN] = {};
+    
+    int max_cpy_len = MAX_PATH_LEN - MAX_FOLDNAME_LEN - 1;
+
+    GetPath(path_from, FOLD_NAME, argv[1], MAX_FOLDNAME_LEN, max_cpy_len);
+    GetPath(path_to,   FOLD_NAME, argv[2], MAX_FOLDNAME_LEN, max_cpy_len);
+
+    *file_from = fopen(path_from, "r");   
+    *file_to = fopen(path_to, "w+");    
 
     if (argc < 3)
     {
@@ -27,4 +36,10 @@ OpenFilesRes OpenGettedFiles(const int argc, const char *argv[], FILE **file_fro
 
     else
         return OPEN_OK;
+}
+
+void GetPath(char *res_path, const char *folder, const char *name, size_t max_foldname_len, size_t max_path_name)
+{
+    strncat(res_path, folder, max_foldname_len);
+    strncat(res_path, name, max_path_name - max_foldname_len - 1);
 }
